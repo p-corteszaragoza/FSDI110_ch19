2ab26844b864615608f742af625af9773590f84c
Paola Cortes <pcorteszaragoza@gmail.com>
2021-07-25 11:32:28 -0700

    Class 2 - MongoDB connection

---
+++ config.py
@@ -0,0 +1,21 @@
+import pymongo
+import json
+from bson import ObjectId
+
+# connection string
+# mongo_url = "mongodb+srv://........"
+mongo_url = "mongodb://localhost:27017"
+client = pymongo.MongoClient(mongo_url)
+
+db = client.get_database("onlinestore")
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+def parse_json(data):
+    return JSONEncoder().encode(data)
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, abort, request, render_template
 from data import data
 import json
 from flask_cors import CORS
+from config import db, parse_json
 
 app = Flask(__name__)
 CORS(app)
@@ -42,7 +43,11 @@ def fullname():
 
 @app.route("/api/catalog")
 def get_catalog():
-    return json.dumps(products)
+    cursor = db.products.find({})
+    catalog = [item for item in cursor]
+    # for item in cursor:
+    #    catalog.append(item)
+    return parse_json(catalog)
 
 # create a POST endpoint
 # to register new products
@@ -51,27 +56,18 @@ def get_catalog():
 @app.route("/api/catalog", methods=['POST'])
 def save_product():
     prod = request.get_json()
-    products.append(prod)
-    return json.dumps(prod)
+    db.products.insert(prod)
+    return parse_json(prod)
+    # products.append(prod)
+    # return json.dumps(prod)
 
 
 @app.route("/api/catalog/<category>")
 def get_product_by_category(category):
-    # find the products that belong to category
-    """
-      create a results list
-      travel the list
-      get each dict inside the list
-      if the dict category is equal to my category var:
-          push the dict into the results
+    data = db.products.find({"category": category})
+    results = [item for item in data]
 
-      at the end, return the resuts as a json
-      """
-    results = []
-    for prod in products:
-        if(prod["category"].lower() == category.lower()):
-            results.append(prod)
-    return "You want prods for " + json.dumps(results)
+    return parse_json(results)
 
 
 @app.route("/api/catalog/id/<id>")
@@ -96,16 +92,25 @@ def get_product_cheapest():
 
 @app.route("/api/categories")
 def get_categories():
+    data = db.products.find({})
     unique_categories = []
-    for prod in products:
+    # for prod in products:
+    for prod in data:
         category = prod["category"]
         if category not in unique_categories:
             unique_categories.append(category)
-    return json.dumps(unique_categories)
+    return parse_json(unique_categories)
 
 
 @app.route("/api/test")
-def test():
+def test_data_manipulation():
+    test_data = db.test.find({})
+    print(test_data)
+    return parse_json(test_data[0])
+
+
+@app.route("/api/test2")
+def test2():
     # add
     products.append("strawberry")
     products.append("dragon fruit")
